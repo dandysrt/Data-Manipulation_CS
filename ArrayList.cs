a07256a6b8370e590fc6b7a3b65663d8b4817a35
Dandy Martin <dandy@siu.edu>
2016-07-11 17:20:17 -0500

    Initial Upload of files
    Initial upload of files

---
+++ ArrayList.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Manipulation
+{
+    class ArrayList<T>
+    {
+        protected T[] list;
+        private int currentPos = 0;
+        private int size = 2;
+
+        public ArrayList()
+        {
+            this.list = new T[size];
+        }
+
+        public ArrayList(T[] list)
+        {
+            this.list = list;
+            currentPos = list.Length;
+            grow();
+        }
+
+        private void shrink()
+        {
+            int count = 0;
+            int length = currentPos < list.Length ? currentPos : list.Length;
+            for(int i = 0; i < length; i++)
+            {
+                if(list[i] == null)
+                {
+                    for(int j = i; j < length - 1; j++)
+                    {
+                        list[j] = list[j + 1];
+                    }
+                    count++;
+                }
+            }
+            currentPos -= count;
+        }
+
+        private void grow()
+        {
+            T[] tempList = new T[(list.Length * 2)];
+            int length = currentPos < list.Length ? currentPos : list.Length;
+            for (int i = 0; i < length; i++)
+            {
+                tempList[i] = list[i];
+            }
+            list = tempList;
+        }
+
+        private void grow(int size)
+        {
+            T[] tempList = new T[(list.Length + size)];
+            int length = currentPos < list.Length ? currentPos : list.Length;
+            for(int i = 0; i < length; i++)
+            {
+                tempList[i] = list[i];
+            }
+            list = tempList;
+        }
+
+        public Boolean add(T data)
+        {
+            try
+            {
+                if(currentPos < list.Length)
+                {
+                    list[currentPos] = data;
+                }else
+                {
+                    grow();
+                    list[currentPos] = data;
+                }
+                currentPos++;
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public void add(int index, T data)
+        {
+            try
+            {
+                if (index >= list.Length || currentPos >= list.Length)
+                    grow();
+                T[] temp = new T[(currentPos - index)];
+                for(int i = 0; i < temp.Length; i++)
+                {
+                    temp[i] = list[(index + i)];
+                }
+                list[index] = data;
+                addAll(temp);
+            }catch(IndexOutOfRangeException e)
+            {
+                Console.Error.Write("Index out of Bounds");
+                e.StackTrace.ToString();
+            }
+        }
+
+        public Boolean addAll(T[] listData)
+        {
+            try
+            {
+                grow(listData.Length);
+                for(int i = 0; i < listData.Length; i++)
+                {
+                    list[(currentPos + i)] = listData[i];
+                }
+                currentPos += listData.Length;
+                return true;
+            }catch(NullReferenceException e)
+            {
+                Console.Error.Write("Provided Array references Null");
+                e.StackTrace.ToString();
+                return false;
+            }
+        }
+
+        public void clear()
+        {
+            list = new T[this.size];
+        }
+
+        public T get(int pos)
+        {
+            try
+            {
+                return list[pos];
+            }catch(IndexOutOfRangeException e)
+            {
+                e.StackTrace.ToString();
+                return default(T); //need to verify syntax
+            }
+        }
+
+        public int indexOf(T data)
+        {
+            int length = currentPos < list.Length ? currentPos : list.Length;
+            for(int i = 0; i < length; i++)
+            {
+                if (list[i].Equals(data))
+                    return i;
+            }
+            return -1;
+        }
+
+        public int lastIndexOf(T data)
+        {
+            int length = currentPos < list.Length ? currentPos : list.Length;
+            int lastKnown = -1;
+            for(int i = 0; i < length; i++)
+            {
+                if (list[i].Equals(data))
+                    lastKnown = i;
+            }
+            return lastKnown;
+        }
+
+        public T remove(int index)
+        {
+            try
+            {
+                T output = list[index];
+                list[index] = default(T); //again check syntax
+                shrink();
+                return output;
+            }catch(IndexOutOfRangeException e)
+            {
+                Console.Error.Write("Index is out of range");
+                e.StackTrace.ToString();
+                return default(T); //syntax ?
+            }
+        }
+
+        protected void removeRange(int startIndex, int lastindex)
+        {
+            try
+            {
+                for(int i = startIndex; i < lastindex; i++)
+                {
+                    list[i] = default(T); //syntax ?
+                }
+                shrink();
+            }catch(IndexOutOfRangeException e)
+            {
+                Console.Error.Write("Range is out of Bounds");
+                e.StackTrace.ToString();
+            }
+        }
+
+        public T[] clone()
+        {
+            return list;
+        }
+
+        Boolean contains(T data)
+        {
+            if (indexOf(data) != -1)
+                return true;
+            return false;
+        }
+
+        public T set(int index, T data)
+        {
+            try
+            {
+                list[index] = data;
+                return list[index];
+            }catch(IndexOutOfRangeException e)
+            {
+                Console.Error.Write("Index out of Range");
+                e.StackTrace.ToString();
+                return default(T); // syntax ?
+            }
+        }
+
+        public int _size()
+        {
+            return this.size = currentPos < list.Length ? currentPos : list.Length;
+        }
+
+        public T[] toArray()
+        {
+            try
+            {
+                T[] tempArray = new T[list.Length];
+                for(int i = 0; i < list.Length; i++)
+                {
+                    tempArray[i] = list[i];
+                }
+                return tempArray;
+            }catch(NullReferenceException e)
+            {
+                Console.Error.Write("Array referneces null");
+                e.StackTrace.ToString();
+                return default(T[]); //syntax ?
+            }
+        }
+
+        public T[] toArray(T[] array)
+        {
+            try
+            {
+                T[] tempArray = new T[array.Length];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    tempArray[i] = list[i];
+                }
+                return tempArray;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.Error.Write("Array Reference is null");
+                e.StackTrace.ToString();
+                return default(T[]); //syntax ?
+            }
+        }
+
+        public void trimToSize()
+        {
+            T[] tempArray = new T[size];
+            for(int i = 0; i < _size(); i++)
+            {
+                tempArray[i] = list[i];
+            }
+            list = tempArray;
+        }
+
+    }
+}
---
+++ HashTable.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Manipulation
+{
+    class HashTable<T>
+    {
+		private LinkList<T>[] hashList;
+		private int listMod;
+		
+		public HashTable(int size){
+			hashList = new LinkList<T>[size];
+			for(int i = 0; i < hashList.Length; i++)
+				hashList[i] = new LinkList<T>();
+			listMod = size;
+		}
+		
+		
+		//can be a rapid shortcut for retrieving data if key has no collisions
+		public T get(int key){
+			LinkList<T> output = hashList[key%listMod];
+			if(output != null){
+				if(output.head == null){
+					return default(T);
+				}else if(output.head.next != null){
+					Console.Out.Write("Multiple values detected at key resolution: %s, "
+							+ " please specify the data you wish to retrieve"
+							+ " USAGE: 'get(key, data)\n",( key%listMod));
+					return default(T);
+				}else{
+					return output.head.data;
+				}
+			}
+			return default(T);
+			
+		}
+		
+		//longest search method if key is unknown O(n^2) at worst
+		public T get(T data){
+			for(int i = 0; i < hashList.Length; i++){	
+				LinkList<T>.Node<T> output = (LinkList<T>.Node<T>)hashList[i].find(data);
+				if(output != null)
+					if(output.data != null)
+						return output.data;
+					
+			}
+			return default(T);
+		}
+		
+		//most rapid search if key is known
+		public T get(int key, T data){
+			return hashList[key%listMod].find(data).data;
+		}
+		
+		public void add(int key, T data){
+			if(hashList[key%listMod] == null)
+				hashList[key%listMod] = new LinkList<T>();
+			hashList[key%listMod].add(key, data);
+		}
+		
+		public void remove(int key, T data){
+			hashList[key%listMod].remove(data);
+		}
+		
+		//unused will be implemented for dynamic list
+		private void updateListMod(){
+			listMod = hashList.Length;
+		}
+		
+		//Doubles the size of the hash table when called
+		public void grow(){
+			LinkList<T>[] tempList = new LinkList<T>[(hashList.Length * 2)];
+			int i ;
+			for( i = 0; i < hashList.Length; i++){
+				tempList[i] = new LinkList<T>();
+				if(hashList[i] != null){
+					LinkList<T>.Node<T> tempNode = (LinkList<T>.Node<T>)hashList[i].head;
+					if(tempNode != null){
+						while(tempNode.data != null){
+							if(tempList[tempNode.key%tempList.Length] == null)
+								tempList[tempNode.key%tempList.Length] = new LinkList<T>();
+							tempList[tempNode.key%tempList.Length].add(tempNode.data);
+							if(tempNode.next != null)
+								tempNode = (LinkList<T>.Node<T>) tempNode.next;
+							else 
+								tempNode.data = default(T);
+						}
+						tempNode = null;
+					}
+				}
+			}
+			hashList = tempList;
+			updateListMod();
+		}
+	
+		public void printTable(){
+			for(int i = 0; i < hashList.Length; i++){
+				if(hashList[i] != null){
+					if(hashList[i].head != null) Console.Out.Write("%s: ", i);
+					hashList[i].printList();
+				}else{
+					hashList[i] = new LinkList<T>();
+				}
+			}
+		}
+		
+		/*=========================SUBCLASSES===========================*/
+		
+		public class LinkList<T> : LinkedList<T>{
+			public void add(int key, T data){
+				if(head == null){
+                    head = new Node<T>(null, key, data, (Node<T>)tail);
+                    tail = head;
+					return;
+				}
+				Node<T> newNode = new Node<T>((Node<T>)tail, key, data, null);
+                tail.next = newNode;
+                tail = newNode;
+			}
+			
+			public class Node<T> : LinkedList<T>.Node<T>{
+				public int key;
+				public Node(Node<T> prev, int key, T data, Node<T> next) : base(prev, data, next){
+					this.key = key;
+				}
+			}
+			
+		}
+    }
+}
